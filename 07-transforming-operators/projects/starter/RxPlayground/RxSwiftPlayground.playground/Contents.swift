64d1ad6da2782845002400cf2836f53c3cb397b4
RandallStevens <kradius@163.com>
2021-09-12 11:50:48 +0800

    feat: accomplish chapter 7 challenge

--- 07-transforming-operators/projects/starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 07-transforming-operators/projects/starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -248,4 +248,119 @@ example(of: "materialize and dematerialize ") {
     student.onNext(jane)
 }
 
+example(of: "Challenge 1") {
+    let disposeBag = DisposeBag()
+    
+    let contacts = [
+        "603-555-1212": "Florent",
+        "212-555-1212": "Shai",
+        "408-555-1212": "Marin",
+        "617-555-1212": "Scott"
+    ]
+    
+    let convert: (String) -> Int? = { value in
+        if let number = Int(value),
+           number < 10 {
+            return number
+        }
+        
+        let keyMap: [String: Int] = [
+            "abc": 2, "def": 3, "ghi": 4,
+            "jkl": 5, "mno": 6, "pqrs": 7,
+            "tuv": 8, "wxyz": 9
+        ]
+        
+        let converted = keyMap
+            .filter { $0.key.contains(value.lowercased()) }
+            .map(\.value)
+            .first
+        
+        return converted
+    }
+    
+    let format: ([Int]) -> String = {
+        var phone = $0.map(String.init).joined()
+        
+        phone.insert("-", at: phone.index(
+                        phone.startIndex,
+                        offsetBy: 3)
+        )
+        
+        phone.insert("-", at: phone.index(
+                        phone.startIndex,
+                        offsetBy: 7)
+        )
+        
+        return phone
+    }
+    
+    let dial: (String) -> String = {
+        if let contact = contacts[$0] {
+            return "Dialing \(contact) (\($0))..."
+        } else {
+            return "Contact not found"
+        }
+    }
+    
+    let input = PublishSubject<String>()
+    
+    // 1.Use multiple maps to perform each transformation along the way.
+    // 2.Use skipWhile just like you did in Chapter 5 to skip 0s at the beginning.
+    // 3.Handle the optionals returned from convert.
+    
+    
+    //    “Your goal for this challenge is to modify this implementation to be able to take letters as well, and convert them to their corresponding number based on a standard phone keypad (abc is 2, def is 3, and so on).”
+    
+    // Add your code here
+    input.asObservable()
+        .debug()
+        .map { convert($0) }
+        .filter { $0 != nil}
+        .map { $0! }
+        .skipWhile { $0 == 0 }
+        .take(10)
+        .toArray()// Converts an Observable into a Single that emits the whole sequence as a single array and then terminates.
+        .map { format($0) }
+        .map {
+            print($0)
+            return dial($0)
+        }
+        .subscribe (
+            onSuccess: { str in
+                print(str)
+            }, onError: { error in
 
+            }
+        )
+        .disposed(by: disposeBag)
+    
+    // 标准答案
+//    input.asObservable()
+//        .map(convert)
+//        .filter { $0 != nil}
+//        .map { $0! }
+//        .skipWhile { $0 == 0 }
+//        .take(10)
+//        .toArray()
+//        .map(format)
+//        .map(dial)
+//        .subscribe(onSuccess: {
+//          print($0)
+//        })
+//        .disposed(by: disposeBag)
+    
+    input.onNext("")
+    input.onNext("0")
+    input.onNext("408")
+    
+    input.onNext("6")
+    input.onNext("")
+    input.onNext("0")
+    input.onNext("3")
+    
+    "JKL1A1B".forEach {
+        input.onNext("\($0)")
+    }
+    
+    input.onNext("9")
+}
